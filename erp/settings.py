60ed18e170b98d0745ac73b9a60920be6cb9dc62
vishnu prasad <mvihnu673@gmail.com>
2026-04-18 17:30:53 +0530

    Initial committtt

--- erp/settings.py
+++ erp/settings.py
@@ -27,6 +27,9 @@ SECRET_KEY = 'django-insecure-gdp^^*e2%+n&zle5_3#(e-w5zorxo+-a2+_4e*yzva^re8-xjc
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com"
+]
 
 
 
