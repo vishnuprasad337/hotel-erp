b2ed7817bfbb2ab66d9dee082ebf40b0053db8f6
vishnu prasad <mvihnu673@gmail.com>
2026-04-21 16:05:31 +0530

    hosting

--- erp/settings.py
+++ erp/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = [
     
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://hotel-erp-24.onrender.com",
+  
     "https://*.onrender.com",
 ]
 
@@ -116,7 +116,7 @@ DATABASES = {
 
 DATABASES['default']['ENGINE'] = 'django_tenants.postgresql_backend'
 IS_RENDER = os.environ.get("RENDER") == "True"
-BASE_URL = "eerp.onrender.com" if IS_RENDER else "localhost"
+BASE_URL = ".onrender.com" if IS_RENDER else "localhost"
 PORT = "" if IS_RENDER else ":8000"
 
 
